00a21f838556fa4e64722e2cd433a313aa223273
Sebastian <sebastianabraham2006@gmail.com>
2025-01-02 18:03:25 +0530

    improved the database connection logic

--- database/postgres.go
+++ database/postgres.go
@@ -2,24 +2,24 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type Config struct {
-	Host     string
-	Port     string
-	Password string
-	User     string
-	DBName   string
-	SSLMode  string
-}
+func NewConnection() (*gorm.DB, error) {
 
-func NewConnection(config *Config) (*gorm.DB, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	dsn := fmt.Sprintf(
 		"host=%s port=%s password=%s user=%s dbname=%s sslmode=%s",
-		config.Host, config.Port, config.Password, config.User, config.DBName, config.SSLMode)
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return db, err
--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
+	"github.com/sebastian-abraham/some-stuff/database"
+	"github.com/sebastian-abraham/some-stuff/routes"
 )
 
-type Repository struct {
-	DB *gorm.DB
-}
-
 func main() {
+
+	db, err := database.NewConnection()
+	if err != nil {
+		log.Fatal("Could not load database")
+	}
 
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Document Section")
 	})
+
+	routes.UserRoutes(db, app)
 
 	app.Listen(":8080")
 }
